e0e8d87343a6c153b80ba9177ba27f67e4559e95
NatRikk <145622298+NatRikk@users.noreply.github.com>
2024-11-26 17:13:39 -0600

    added instruction from Canvas in the README, added all the files from the "Start Code Folder" on Canvas

---
+++ weather.js
@@ -0,0 +1,8 @@
+/* Put your scripts in this file. */
+/* Remember:
+   1. Once the page is loaded, wire up your button so
+      it can process the zip code entered.   
+   2. The handler for the button click will need
+      to make an AJAX call to retrieve the data from
+      OpenWeatherMap.com.
+*/
