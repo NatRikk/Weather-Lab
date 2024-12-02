062ba6cd7166f27656b7e8b09d5ee9bab415749b
Austin Metzler <ametzler@drury.edu>
2024-12-02 10:34:45 -0700

    Max is double dumb

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function navigateTo(page) {
     window.location.href = `../pages/${page}`;
   }
-  
+function submitSignup(event) {
+// Prevent the default form submission behavior
+event.preventDefault();  
+// Here, you could validate the form or save data if needed
+// Redirect to the dashboard page
+navigateTo('dashboard.html');
+}    
