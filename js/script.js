461d13c2775981c6cb5dbba217501c2dfa6b2bd2
MaxHagl <maximilian.hagl@gmail.com>
2024-12-02 11:39:52 -0600

    added confirm passowrd verification

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ event.preventDefault();
 // Redirect to the dashboard page
 navigateTo('dashboard.html');
 }    
+
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+  const errorMessage = document.getElementById('error-message');
+
+  if (password !== confirmPassword) {
+      event.preventDefault(); // Prevent form submission
+      errorMessage.textContent = 'Passwords do not match!';
+  } else {
+      errorMessage.textContent = ''; // Clear error message
+  }
+});
